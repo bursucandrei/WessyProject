a0e0d8a3328bb2548e98495615365b6f95d61ab0
Bursuc Andrei <henry_14yo@yahoo.com>
2013-06-20 14:47:11 +0300

    schimbare fotografii

--- js/wessy.js
+++ js/wessy.js
@@ -1,3 +1,5 @@
+var randomnr;
+var img;
 function formRegister()
 {
 $.ajax({
@@ -14,13 +16,29 @@ $.ajax({
 
 function hideDiv()
 {
-$('#hideImg').remove();
+randomnr=Math.floor(Math.random()*26)
+img = new Image(); // width, height values are optional params 
+img.src = 'img/gameimg/pic'+randomnr+'.jpg';
+img.alt='Personajul de ghicit';
+img.className='img-polaroid';
+img.id='divNewImg';
+$('#alertDiv').text('Which one of this people is in this picture?');
+$('#hideImg').replaceWith(img);
 $('#btnClose').remove();
 $('#btnPlay').replaceWith($('#btnConfirm'));
 $('#btnDetails').replaceWith($('#btnHint'));
 $('#btnCustomize').replaceWith($('#btnSkip'));
 $('#btnFb').replaceWith($('#btnFacebook'));
 $('#btnQu').replaceWith($('#btnQuit'));
+}
 
-
+function onclick_Confirm()
+{
+randomnr=Math.floor(Math.random()*26)
+var imgNew = new Image(); // width, height values are optional params 
+imgNew.src = 'img/gameimg/pic'+randomnr+'.jpg';
+imgNew.alt='Personajul de ghicit';
+imgNew.className='img-polaroid';
+$('#divNewImg').replaceWith(imgNew);
+imgNew.id='divNewImg';
 }
